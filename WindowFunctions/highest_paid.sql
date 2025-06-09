e2fa0946f9b85c3b55f54414d58920e61a8e4a5c
Ishani Lahiri <ishanirocks323@gmail.com>
2025-06-09 19:04:44 +0530

    Update highest_paid.sql

--- WindowFunctions/highest_paid.sql
+++ WindowFunctions/highest_paid.sql
@@ -1,1 +1,12 @@
-
+--top 3 highest paid employee in each department
+with cte as
+(
+select e.employeeid, e.firstname, e.lastname,d.departmentname, e.salary,
+rank() over (partition by d.departmentname order by e.salary desc) as rnk
+from Departments d
+inner join Employees e
+on d.departmentid=e.departmentid
+)
+select employeeid, firstname, lastname, departmentname, salary
+from cte 
+where rnk=1
